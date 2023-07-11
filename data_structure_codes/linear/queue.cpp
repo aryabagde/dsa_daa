f841cf43dc1c38ec0e288c63db0a117bdf046fe9
aryabagde <akruyhau@gmail.com>
2023-07-11 23:52:46 +0530

    completing queue

--- data_structure_codes/linear/queue.cpp
+++ data_structure_codes/linear/queue.cpp
@@ -4,17 +4,27 @@ using namespace std;
 
 class Queue{
 	private:
-		int front;
-		int rear;
-		int arr[5];
+		int front;           //font head of the queue
+		int rear;            //rear head of the queue
+		int arr[5];          //static initialization
 		
 	public:
-		Queue(){
+		Queue(){             // constructor function
+			
+			front = -1;      //no values in queue so front and rear values are -1
+			rear = -1;
+			for(int i=0; i<5; i++){  // initializing all the values to 0
+				
+				arr[i] = 0;
+			}
 			
 		}
 };
 
 int main(){
+	
+	Queue q1;
+	
 	do{
 		
 		cout<<"What operation do you want to perform?"<<endl;
